bacf9824b1ecc8766a4bade9c02443a795cefcaa
Arseniy <belovas01@mail.ru>
2022-01-23 11:04:49 +0700

    asd

--- public/app.js
+++ public/app.js
@@ -6,3 +6,31 @@ $(function () {
         $('#register').addClass('show active')
     }
 })
+let clicks = 0
+$(".mobile-result-box").on("click", function () {
+    clicks++
+    if (clicks % 2 !== 0) {
+        $(this).find(".mobile-box-before").addClass('animation').css("zIndex", '50')
+        $(this).find(".mobile-box-after").removeClass('animation').css("zIndex", '40')
+    } else {
+        $(this).find(".mobile-box-before").removeClass('animation').css("zIndex", '40')
+        $(this).find(".mobile-box-after").addClass('animation').css("zIndex", '50')
+    }
+})
+// $(".mobile-result-box").on("click", function (){
+//     $(".mobile-box-before").animate({
+//         "z-index" : "5"
+//     }, 1000);
+//     $(".mobile-box-after").animate({
+//         "z-index" : "4"
+//     }, 1000);
+// })
+
+// const boxAnimate = document.querySelector('mobile-result-box');
+// const boxBefore = document.querySelector('mobile-box-before')
+// const boxAfter = document.querySelector('mobile-box-after')
+//
+// boxAnimate.addEventListener('click', event => {
+//     boxBefore.style.transform = 'translateX(50%)',
+//     boxBefore.style.zIndex = `30`;
+// });
